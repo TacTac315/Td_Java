13ad3362028a1b7e51fa74b9e82f08f3761c3d09
Vincent Eychenne <veychenn@etud.insa-toulouse.fr>
2023-03-08 16:48:17 +0100

    Exercice 31 complet

---
+++ src/partie3/Exercice31.java
@@ -0,0 +1,19 @@
+package partie3;
+
+public class Exercice31
+{
+
+	public static void main(String[] argv) {
+		Voiture E350;
+		E350 = new Voiture();
+		Voiture SLK230 = new Voiture();
+		E350.m_puissance = 292;
+		E350.m_vitesse = 260;
+		E350.m_estDemarree = false;
+
+	
+		SLK230.m_puissance = 292;
+		SLK230.m_vitesse = 240;
+		SLK230.m_estDemarree = false;
+}
+}
---
+++ src/partie3/Voiture.java
@@ -0,0 +1,37 @@
+package partie3;
+
+public class Voiture {
+	public int m_puissance;
+	public int m_vitesse;
+	public boolean m_estDemarree;
+	
+	//Constructeurs (pas de voiture)
+	
+	public Voiture() {
+		m_puissance=0;
+		m_vitesse=0;
+		m_estDemarree= false;
+	}
+	
+	public Voiture(int puissance) {
+		m_puissance=puissance;
+		m_vitesse=0;
+		m_estDemarree=false;
+	}
+	
+	public Voiture(int puissance, int vitesse) {
+		m_puissance=puissance;
+		m_vitesse=vitesse;
+		m_estDemarree=false;
+	}
+	
+	public Voiture(int puissance, int vitesse, boolean estDemarree) {
+		m_puissance=puissance;
+		m_vitesse=vitesse;
+		m_estDemarree=estDemarree;
+	}
+	
+}
+
+
+
